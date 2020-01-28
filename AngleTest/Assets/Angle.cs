e5d2fff09fda46b36e9214bfaeb7023a366dc1ca
Adrian-HO <54951013+Adrian-HO@users.noreply.github.com>
2020-01-28 14:25:34 -0500

    trip

---
+++ AngleTest/Assets/Angle.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Angle : MonoBehaviour {
+    public GameObject target;
+    public float fov = 20.0f;
+    public Material CanSee;
+    public Material CannotSee;
+    public float ang;
+    public float distancesquared;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetdir = target.transform.position - transform.position;
+        distancesquared = targetdir.sqrMagnitude;
+        ang = Vector3.Angle(targetdir, transform.forward);
+        if (ang < fov && distancesquared < 100)
+        {
+            target.GetComponent<Renderer>().material = CanSee;
+        }
+        else
+        {
+            target.GetComponent<Renderer>().material = CannotSee;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        // we want to draw a line along FOV
+        Vector3 f = transform.forward*10;
+        Vector3 f1 = Quaternion.AngleAxis(fov, Vector3.up) * f;
+        Vector3 f2 = Quaternion.AngleAxis(-fov, Vector3.up) * f;
+        Vector3 f3 = Quaternion.AngleAxis(fov, Vector3.right) * f;
+        Vector3 f4 = Quaternion.AngleAxis(-fov, Vector3.right) * f;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, f);
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, f1);
+        Gizmos.DrawRay(transform.position, f2);
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, f3);
+        Gizmos.DrawRay(transform.position, f4);
+
+
+    }
+}
---
+++ AngleTest/Assets/Drone.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drone : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 60.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(transform.right, rotationSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= transform.forward * speed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(transform.up, rotationSpeed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(transform.up, -rotationSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(transform.forward, rotationSpeed * Time.deltaTime, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(transform.forward, -rotationSpeed * Time.deltaTime, Space.Self);
+        }
+    }
+}
